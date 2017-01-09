3ec591392cc8aa7192950563c70aade6366917c4
Michael <michaelfiford@gmail.com>
2017-01-09 18:46:51 +0000

    Attempt to fix react

--- EvotoApi/Areas/Management/Web/index.jsx
+++ EvotoApi/Areas/Management/Web/index.jsx
@@ -1,10 +1,10 @@
 ﻿import React from "react"
 import ReactDOM from "react-dom"
-import {Router, Route, useRouterHistory, IndexRoute, createHashHistory} from "react-router"
+import {Router, Route, useRouterHistory, IndexRoute} from "react-router"
+import {createHashHistory} from "history"
 import App from "./App.jsx"
 import Home from "./components/Home.jsx"
 import LoginContent from './components/LoginContent.jsx'
-
 
 const history = useRouterHistory(createHashHistory)({
     basename: '/management'
