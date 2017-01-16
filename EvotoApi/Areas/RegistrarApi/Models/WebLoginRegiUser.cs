0c1d7d00a05a5fa33f46e63e6d76521940427ddf
Michael <michaelfiford@gmail.com>
2017-01-16 17:59:03 +0000

    Add account routes and password logic. Copy scripts for Registrar

---
+++ EvotoApi/Areas/RegistrarApi/Models/WebLoginRegiUser.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.Runtime.Serialization;
+
+namespace EvotoApi.Areas.RegistrarApi.Models
+{
+    [Serializable]
+    public class WebLoginRegiUser
+    {
+        [DataMember(Name = "email")]
+        [Required]
+        public string Email { get; }
+
+        [DataMember(Name = "password")]
+        [Required]
+        public string Password { get; }
+    }
+}
--- EvotoApi/Controllers/HomeController.cs
+++ EvotoApi/Controllers/HomeController.cs
@@ -1,12 +1,11 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.Mvc;
+﻿using System.Web.Mvc;
 
-namespace EvotoApi.Controllers {
-    public class HomeController : Controller {
-        public ActionResult Index() {
+namespace EvotoApi.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
             ViewBag.Title = "Home Page";
 
             return View();
