11eecfac885452f158002a2e54b3e913f602c539
Michael <michaelfiford@gmail.com>
2017-01-17 14:22:31 +0000

    Missing files from last commit

--- Management.Database/Management.Migrate.bat
+++ Management.Database/Management.Migrate.bat
@@ -1,10 +1,4 @@
 set MSB="%ProgramFiles(x86)%\MSBuild\14.0\Bin\MSBuild.exe"
-
-if "%1" == "" (
-  set CONFIG=..\EvotoApi\Web.config
-) ELSE (
-  set CONFIG=..\EvotoApi\Web.%1.config
-)
-
+set CONFIG=..\EvotoApi\Web.config
 
 %MSB% Management.Migrate.proj /t:Migrate /p:DatabaseProvider=SqlServer2012 /p:ConnectionStringConfigPath=%CONFIG% /p:ConnectionStringName=ManagementConnectionString /p:DataMigrationProjectName=Management.Database /p:DataMigrationProjectRootPath=. /p:MigratorTasksDirectory=..\packages\FluentMigrator.1.6.2\tools\
--- Registrar.Database/Registrar.Migrate.bat
+++ Registrar.Database/Registrar.Migrate.bat
@@ -1,10 +1,4 @@
 set MSB="%ProgramFiles(x86)%\MSBuild\14.0\Bin\MSBuild.exe"
-
-if "%1" == "" (
-  set CONFIG=..\EvotoApi\Web.config
-) ELSE (
-  set CONFIG=..\EvotoApi\Web.%1.config
-)
-
+set CONFIG=..\EvotoApi\Web.config
 
 %MSB% Registrar.Migrate.proj /t:Migrate /p:DatabaseProvider=SqlServer2012 /p:ConnectionStringConfigPath=%CONFIG% /p:ConnectionStringName=RegistrarConnectionString /p:DataMigrationProjectName=Registrar.Database /p:DataMigrationProjectRootPath=. /p:MigratorTasksDirectory=..\packages\FluentMigrator.1.6.2\tools\
