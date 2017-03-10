c87a39c7f7b79b41d81060a62b6c8fbf2fe37d61
Matthew Elphick <matt.a.elphy@gmail.com>
2017-03-10 00:17:32 +0000

    Remove custom authorize for ReactController

--- EvotoApi/Areas/Management/Controllers/ReactController.cs
+++ EvotoApi/Areas/Management/Controllers/ReactController.cs
@@ -4,22 +4,6 @@ using System.Web.Mvc;
 
 namespace EvotoApi.Areas.Management.Controllers
 {
-    [AttributeUsage(AttributeTargets.Class | AttributeTargets.Method, Inherited = true, AllowMultiple = true)]
-    public class AuthorizeAttribute : System.Web.Mvc.AuthorizeAttribute
-    {
-        protected override void HandleUnauthorizedRequest(AuthorizationContext filterContext)
-        {
-            if (filterContext.HttpContext.Request.IsAuthenticated)
-            {
-                filterContext.Result = new HttpStatusCodeResult((int)System.Net.HttpStatusCode.Forbidden);
-            }
-            else
-            {
-                filterContext.Result = new RedirectResult("/manage/login");
-            }
-        }
-    }
-
     public class ReactController : Controller
     {
         [Authorize]
