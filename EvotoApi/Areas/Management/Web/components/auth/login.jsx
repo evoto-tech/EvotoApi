13ab4d7cdfb75f2ddf63be68602922bf504f46e6
Matthew Elphick <matt.a.elphy@gmail.com>
2017-03-10 18:13:01 +0000

    Fix login/register, now shows error messages

--- EvotoApi/Areas/Management/Web/components/auth/login.jsx
+++ EvotoApi/Areas/Management/Web/components/auth/login.jsx
@@ -8,7 +8,8 @@ class Login extends React.Component {
       email: '',
       password: '',
       ready: false,
-      error: ''
+      error: '',
+      errorMessages: []
     }
   }
 
@@ -23,11 +24,21 @@ class Login extends React.Component {
     this.setState(update, cb || this.checkReady)
   }
 
-  login () {
+  login (e) {
+    e.preventDefault()
     let user = {
           email: this.state.email,
           password: this.state.password
         }
+
+    let handleError = (err, errorMessages) => {
+      errorMessages = errorMessages || []
+      console.error('Login error', err, errorMessages)
+      this.setState({
+        error: 'Those details seem to be wrong! Please try again.',
+        errorMessages
+      })
+    }
 
     fetch('/mana/auth/login',
       { method: 'POST',
@@ -38,74 +49,95 @@ class Login extends React.Component {
           'Content-Type': 'application/json; charset=utf-8'
         }
       })
+      .then((response) => {
+        if (response.ok && response.status === 200) {
+          return this.props.router.push('/')
+        } else if (response.status === 401) {
+          return handleError(response)
+        } else if (response.status === 400) {
+          return response.json()
+        } else {
+          return handleError(response)
+        }
+      }, handleError)
       .then((data) => {
-        this.props.router.push('/')
-      }, console.error)
-      .catch((err) => {
-        console.error('Login error', err)
-        this.setState({ error: 'Those details seem to be wrong! Please try again.' })
-      })
+        let messages = data.ModelState
+          ? Object.keys(data.ModelState).reduce((o, k) => data.ModelState[k], [])
+          : undefined
+        return handleError(data, messages)
+      }, handleError)
+      .catch(handleError)
   }
 
   render () {
+    let errorMessages = this.state.errorMessages.length > 0 ? (
+      <ul>
+        {this.state.errorMessages.map((message, i) => (
+          <li key={i}>{message}</li>
+        ))}
+      </ul>
+    ) : ''
     return (
       <div>
-        { this.state.error === '' ? '' : (
-            <div className='callout callout-danger'>
-              <h4>Error!</h4>
-              <p>{this.state.error}</p>
-            </div>
-          )
-        }
-        <div className='input-group' style={{ width: '100%' }}>
-          <span className='fa fa-envelope input-group-addon' style={{ width: '40px' }}/>
-          <input
-            type='text'
-            data-field='email'
-            ref='email'
-            className='form-control'
-            placeholder='Email'
-            onChange={this.onFieldChange.bind(this)}
-            />
-        </div>
-        <br/>
-        <div className='input-group'style={{ width: '100%' }}>
-          <span className='fa fa-lock input-group-addon' style={{ width: '40px' }} />
-          <input
-            type='password'
-            data-field='password'
-            ref='password'
-            className='form-control'
-            placeholder='Password'
-            onChange={this.onFieldChange.bind(this)}
-            />
-        </div>
-        <br/>
-        {/* <div className="row">
-              <div className="col-xs-8">
-                  <div className="checkbox">
-                      <label className="">
-                          <div className="" aria-checked="false" aria-disabled="false" style={{position: "relative"}}>
-                          <input type="checkbox"/>
-                          </div> Remember Me
-                      </label>
-                  </div>
+        <form onSubmit={this.login.bind(this)}>
+          { this.state.error === '' ? '' : (
+              <div className='callout callout-danger'>
+                <h4>Error!</h4>
+                <p>{this.state.error}</p>
+                {errorMessages}
               </div>
-            </div>
-        */}
-        <div className='row'>
-          <div className='col-xs-4'>
-            <button
-              type='submit'
-              disabled={!this.state.ready}
-              className='btn btn-success btn-block btn-flat'
-              onClick={this.login.bind(this)}
-            >
-              Log In
-            </button>
+            )
+          }
+          <div className='input-group' style={{ width: '100%' }}>
+            <span className='fa fa-envelope input-group-addon' style={{ width: '40px' }}/>
+            <input
+              type='text'
+              data-field='email'
+              ref='email'
+              className='form-control'
+              placeholder='Email'
+              onChange={this.onFieldChange.bind(this)}
+              />
           </div>
-        </div>
-        {/* <a href="#">I forgot my password</a><br /> */}
+          <br/>
+          <div className='input-group'style={{ width: '100%' }}>
+            <span className='fa fa-lock input-group-addon' style={{ width: '40px' }} />
+            <input
+              type='password'
+              data-field='password'
+              ref='password'
+              className='form-control'
+              placeholder='Password'
+              onChange={this.onFieldChange.bind(this)}
+              />
+          </div>
+          <br/>
+          {/* <div className="row">
+                <div className="col-xs-8">
+                    <div className="checkbox">
+                        <label className="">
+                            <div className="" aria-checked="false" aria-disabled="false" style={{position: "relative"}}>
+                            <input type="checkbox"/>
+                            </div> Remember Me
+                        </label>
+                    </div>
+                </div>
+              </div>
+          */}
+          <div className='row'>
+            <div className='col-xs-4'>
+              <button
+                type='submit'
+                disabled={!this.state.ready}
+                className='btn btn-success btn-block btn-flat'
+                onClick={this.login.bind(this)}
+              >
+                Log In
+              </button>
+            </div>
+          </div>
+          {/* <a href="#">I forgot my password</a><br /> */}
+        </form>
       </div>
     )
   }
--- EvotoApi/Areas/Management/Web/components/auth/register.jsx
+++ EvotoApi/Areas/Management/Web/components/auth/register.jsx
@@ -12,7 +12,8 @@ class Register extends React.Component {
       confirmPassword: '',
       matchingPassword: true,
       ready: false,
-      error: ''
+      error: '',
+      errorMessages: []
     }
   }
 
@@ -38,7 +39,8 @@ class Register extends React.Component {
     })
   }
 
-  register () {
+  register (e) {
+    e.preventDefault()
     let user = {
       FirstName: this.state.firstName,
       LastName: this.state.lastName,
@@ -46,6 +48,16 @@ class Register extends React.Component {
       Password: this.state.password,
       ConfirmPassword: this.state.confirmPassword
     }
+
+    let handleError = (err, errorMessages) => {
+      errorMessages = errorMessages || []
+      console.error('Login error', err, errorMessages)
+      this.setState({
+        error: 'Those details seem to be wrong! Please try again.',
+        errorMessages
+      })
+    }
+
     fetch('/api/Account/Register',
       { method: 'POST',
         body: JSON.stringify(user),
@@ -54,112 +66,135 @@ class Register extends React.Component {
           'Content-Type': 'application/json'
         }
       })
-      .then(() => {
-        this.props.router.push('/login')
-      })
-      .catch((err) => {
-        this.setState({ error: err.Message })
-      })
+      .then((response) => {
+        if (response.ok && response.status === 200) {
+          return this.props.router.push('/login')
+        } else if (response.status === 401) {
+          return handleError(response)
+        } else if (response.status === 400) {
+          return response.json()
+        } else {
+          return handleError(response)
+        }
+      }, handleError)
+      .then((data) => {
+        let messages = data.ModelState
+          ? Object.keys(data.ModelState).reduce((o, k) => data.ModelState[k], [])
+          : undefined
+        return handleError(data, messages)
+      }, handleError)
+      .catch(handleError)
   }
 
   render () {
+    let errorMessages = this.state.errorMessages.length > 0 ? (
+      <ul>
+        {this.state.errorMessages.map((message, i) => (
+          <li key={i}>{message}</li>
+        ))}
+      </ul>
+    ) : ''
+
     return (
       <div>
-        { this.state.error === '' ? '' : (
-            <div className='callout callout-danger'>
-              <h4>Error!</h4>
-              <p>{this.state.error}</p>
-            </div>
-          )
-        }
-        <div className='input-group' style={{ width: '100%' }}>
-          <span className='fa fa-user input-group-addon' style={{ width: '40px' }}/>
-          <input
-            type='text'
-            ref='firstName'
-            data-field='firstName'
-            className='form-control'
-            placeholder='First Name'
-            value={this.state.firstName}
-            onChange={this.onFieldChange.bind(this)}
-            />
-        </div>
-        <br/>
-        <div className='input-group' style={{ width: '100%' }}>
-          <span className='fa fa-user input-group-addon' style={{ width: '40px' }}/>
-          <input
-            type='text'
-            ref='lastName'
-            data-field='lastName'
-            className='form-control'
-            placeholder='Last Name'
-            value={this.state.lastName}
-            onChange={this.onFieldChange.bind(this)}
-            />
-        </div>
-        <br/>
-        <div className='input-group' style={{ width: '100%' }}>
-          <span className='fa fa-envelope input-group-addon' style={{ width: '40px' }}/>
-          <input
-            type='text'
-            ref='email'
-            data-field='email'
-            className='form-control'
-            placeholder='Email'
-            value={this.state.email}
-            onChange={this.onFieldChange.bind(this)}
-            />
-        </div>
-        <br/>
-        <div className='input-group'style={{ width: '100%' }}>
-          <span className='fa fa-lock input-group-addon' style={{ width: '40px' }} />
-          <input
-            type='password'
-            ref='password'
-            data-field='password'
-            className='form-control'
-            placeholder='Password'
-            value={this.state.password}
-            onChange={this.checkPassword.bind(this)}
-            />
-        </div>
-        <br/>
-        <div className={`input-group ${this.state.matchingPassword ? '' : 'has-error' }`} style={{ width: '100%' }}>
-          <span className='fa fa-lock input-group-addon' style={{ width: '40px' }} />
-          <input
-            type='password'
-            ref='confirmPassword'
-            data-field='confirmPassword'
-            className='form-control'
-            placeholder='Confirm password'
-            value={this.state.confirmPassword}
-            onChange={this.checkPassword.bind(this)}
-            />
-        </div>
-        {this.state.matchingPassword ? '' : (
-          <div className='input-group has-error'style={{ width: '100%' }}>
-            <span className='has-error help-block'>Passwords do not match</span>
-          </div>
-        )}
-        <br/>
-        {/* <div className="row">
-              <div className="col-xs-8">
-                  <div className="checkbox">
-                      <label className="">
-                          <div className="" aria-checked="false" aria-disabled="false" style={{position: "relative"}}>
-                          <input type="checkbox"/>
-                          </div> Remember Me
-                      </label>
-                  </div>
+        <form onSubmit={this.register.bind(this)}>
+          { this.state.error === '' ? '' : (
+              <div className='callout callout-danger'>
+                <h4>Error!</h4>
+                <p>{this.state.error}</p>
+                {errorMessages}
               </div>
-            </div>
-        */}
-        <div className='row'>
-          <div className='col-xs-4'>
-            <button type='submit' disabled={!this.state.ready} className='btn btn-success btn-block btn-flat' onClick={this.register.bind(this)}>Register</button>
+            )
+          }
+          <div className='input-group' style={{ width: '100%' }}>
+            <span className='fa fa-user input-group-addon' style={{ width: '40px' }}/>
+            <input
+              type='text'
+              ref='firstName'
+              data-field='firstName'
+              className='form-control'
+              placeholder='First Name'
+              value={this.state.firstName}
+              onChange={this.onFieldChange.bind(this)}
+              />
           </div>
-        </div>
-        {/* <a href="#">I forgot my password</a><br /> */}
+          <br/>
+          <div className='input-group' style={{ width: '100%' }}>
+            <span className='fa fa-user input-group-addon' style={{ width: '40px' }}/>
+            <input
+              type='text'
+              ref='lastName'
+              data-field='lastName'
+              className='form-control'
+              placeholder='Last Name'
+              value={this.state.lastName}
+              onChange={this.onFieldChange.bind(this)}
+              />
+          </div>
+          <br/>
+          <div className='input-group' style={{ width: '100%' }}>
+            <span className='fa fa-envelope input-group-addon' style={{ width: '40px' }}/>
+            <input
+              type='text'
+              ref='email'
+              data-field='email'
+              className='form-control'
+              placeholder='Email'
+              value={this.state.email}
+              onChange={this.onFieldChange.bind(this)}
+              />
+          </div>
+          <br/>
+          <div className='input-group'style={{ width: '100%' }}>
+            <span className='fa fa-lock input-group-addon' style={{ width: '40px' }} />
+            <input
+              type='password'
+              ref='password'
+              data-field='password'
+              className='form-control'
+              placeholder='Password'
+              value={this.state.password}
+              onChange={this.checkPassword.bind(this)}
+              />
+          </div>
+          <br/>
+          <div className={`input-group ${this.state.matchingPassword ? '' : 'has-error' }`} style={{ width: '100%' }}>
+            <span className='fa fa-lock input-group-addon' style={{ width: '40px' }} />
+            <input
+              type='password'
+              ref='confirmPassword'
+              data-field='confirmPassword'
+              className='form-control'
+              placeholder='Confirm password'
+              value={this.state.confirmPassword}
+              onChange={this.checkPassword.bind(this)}
+              />
+          </div>
+          {this.state.matchingPassword ? '' : (
+            <div className='input-group has-error'style={{ width: '100%' }}>
+              <span className='has-error help-block'>Passwords do not match</span>
+            </div>
+          )}
+          <br/>
+          {/* <div className="row">
+                <div className="col-xs-8">
+                    <div className="checkbox">
+                        <label className="">
+                            <div className="" aria-checked="false" aria-disabled="false" style={{position: "relative"}}>
+                            <input type="checkbox"/>
+                            </div> Remember Me
+                        </label>
+                    </div>
+                </div>
+              </div>
+          */}
+          <div className='row'>
+            <div className='col-xs-4'>
+              <button type='submit' disabled={!this.state.ready} className='btn btn-success btn-block btn-flat' onClick={this.register.bind(this)}>Register</button>
+            </div>
+          </div>
+          {/* <a href="#">I forgot my password</a><br /> */}
+        </form>
       </div>
     )
   }
