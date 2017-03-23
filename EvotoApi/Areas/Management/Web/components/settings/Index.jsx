eb33e496f2cfbd888c7e6092649fc0fa471e217e
Matthew Elphick <matt.a.elphy@gmail.com>
2017-03-23 19:00:39 +0000

    Add empty settings page

---
+++ EvotoApi/Areas/Management/Web/components/settings/Index.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import { withRouter, Link } from 'react-router'
+import Wrapper from '../parts/Wrapper.jsx'
+import LoadableOverlay from '../parts/LoadableOverlay.jsx'
+
+class SettingsIndex extends React.Component {
+  propTypes: {
+
+  }
+
+  render () {
+    return (
+      <Wrapper
+        title="Settings"
+        description="Settings for the user account."
+        >
+
+      </Wrapper>
+    )
+  }
+}
+
+export default SettingsIndex
