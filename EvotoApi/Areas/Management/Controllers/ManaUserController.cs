98358bfbbc32127087fef2cc02f28d7481cee9f3
Matthew Elphick <matt.a.elphy@gmail.com>
2017-03-23 19:02:44 +0000

    Add mana/user/list route to management api
    Also add single mana user response transport model.

---
+++ EvotoApi/Areas/Management/Controllers/ManaUserController.cs
@@ -0,0 +1,39 @@
+﻿using System.Linq;
+using System.Threading.Tasks;
+using System.Web.Http;
+using Common.Exceptions;
+using EvotoApi.Areas.ManagementApi.Models.Response;
+using Management.Database.Interfaces;
+
+namespace EvotoApi.Areas.Management.Controllers
+{
+    [RoutePrefix("mana/user")]
+    public class ManaUserController : ApiController
+    {
+        private readonly IManaUserStore _store;
+
+        public ManaUserController(IManaUserStore userStore)
+        {
+            _store = userStore;
+        }
+
+        /// <summary>
+        /// Get a list of management users
+        /// </summary>
+        [HttpGet]
+        [Route("list")]
+        public async Task<IHttpActionResult> UserList()
+        {
+            try
+            {
+                var users = await _store.GetUsers();
+                var response = users.Select((v) => new SingleManaUserResponse(v)).ToList();
+                return Json(response);
+            }
+            catch (RecordNotFoundException)
+            {
+                return Json(new object[] { });
+            }
+        }
+    }
+}
