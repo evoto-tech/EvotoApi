d036921ec023f0eaea4d4856d796d3c9ac6b9590
Matthew Elphick <matt.a.elphy@gmail.com>
2017-03-23 19:00:59 +0000

    Add admin react components

---
+++ EvotoApi/Areas/Management/Web/components/admin/New.jsx
@@ -0,0 +1,31 @@
+import React from 'react'
+import { withRouter, Link } from 'react-router'
+import Wrapper from './parts/Wrapper.jsx'
+import LoadableOverlay from '../parts/LoadableOverlay.jsx'
+import Register from '../auth/Register.jsx'
+
+class AdminNew extends React.Component {
+  propTypes: {
+
+  }
+
+  render () {
+    return (
+      <Wrapper
+        title="New Administrator"
+        description="Add a new user to the management site."
+        >
+        <div className='box box-success'>
+          <div className='box-header with-border'>
+            <h3 className='box-title'>New Administrator Details</h3>
+          </div>
+          <div className='box-body'>
+            <Register successLink='/manage/users/list' buttonText='Create New' />
+          </div>
+        </div>
+      </Wrapper>
+    )
+  }
+}
+
+export default AdminNew
---
+++ EvotoApi/Areas/Management/Web/components/admin/parts/Wrapper.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import { withRouter, Link } from 'react-router'
+import Wrapper from '../../parts/Wrapper.jsx'
+
+class UserWrapper extends React.Component {
+  propTypes: {
+    title: React.PropTypes.string.isRequired,
+    description: React.PropTypes.string.isRequired
+  }
+
+  render () {
+    let title = this.props.title || '',
+        description = this.props.description || ''
+    return (
+      <Wrapper title={title} description={description} breadcrumbs={(
+        [<Link to='/admins/new'><i className='fa fa-plus' />New Administrator</Link>]
+      )}>
+        {this.props.children}
+      </Wrapper>
+    )
+  }
+}
+
+export default UserWrapper
