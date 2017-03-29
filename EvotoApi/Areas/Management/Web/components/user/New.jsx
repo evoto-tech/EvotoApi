290e2c6014894c5bbdcc04f86b0eedcab1af7646
Matthew Elphick <matt.a.elphy@gmail.com>
2017-03-29 20:11:06 +0100

    Rework register components

--- EvotoApi/Areas/Management/Web/components/user/New.jsx
+++ EvotoApi/Areas/Management/Web/components/user/New.jsx
@@ -1,12 +1,30 @@
 import React from 'react'
 import Wrapper from './parts/Wrapper.jsx'
+import RegisterForm from '../parts/forms/Register.jsx'
 
 class UserNew extends React.Component {
+  register(user, successLink, handleError) {
+    console.log('what', user)
+  }
+
   render () {
     let title = 'New User'
     let description = 'New User'
     return (
-      <Wrapper title={title} description={description} />
+      <Wrapper title={title} description={description}>
+        <div className='box box-success'>
+          <div className='box-header with-border'>
+            <h3 className='box-title'>New User Details</h3>
+          </div>
+          <div className='box-body'>
+            <RegisterForm
+              onProcess={this.register.bind(this)}
+              successLink='/users'
+              buttonText='Create New User'
+            />
+          </div>
+        </div>
+      </Wrapper>
     )
   }
 }
