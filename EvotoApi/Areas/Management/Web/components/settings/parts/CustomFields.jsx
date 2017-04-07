84055f424abf78ff3387f6b0a8ed81abf7fc11d4
Matthew Elphick <matt.a.elphy@gmail.com>
2017-04-07 20:31:10 +0100

    Rename options to answers, change to use state utilities

---
+++ EvotoApi/Areas/Management/Web/components/settings/parts/CustomFields.jsx
@@ -0,0 +1,119 @@
+import React from 'react'
+import Box from '../../parts/Box.jsx'
+import LoadableOverlay from '../../parts/LoadableOverlay.jsx'
+import CustomField from './parts/CustomField.jsx'
+import { insert, update, remove } from '../../../lib/state-utils'
+
+class CustomFields extends React.Component {
+  constructor (props) {
+    super(props)
+    this.state = { loaded: false, customFields: [] }
+  }
+
+  componentDidMount () {
+    fetch(`/regi/settings/custom-fields`, { credentials: 'same-origin' })
+      .then((res) => res.json())
+      .then(this.cleanValidationJson)
+      .then((data) => {
+        this.setState({ customFields: data, loaded: true })
+      })
+      .catch(console.error)
+  }
+
+  swalSaveErrorAlert () {
+    swal({
+      title: 'Error',
+      text: 'There was a problem saving the custom fields. Your changes have not been saved.',
+      type: 'error',
+      confirmButtonColor: '#d73925',
+      confirmButtonText: 'Close',
+      closeOnConfirm: true,
+      allowOutsideClick: true
+    })
+  }
+
+  addCustomField (e) {
+    e.preventDefault()
+    this.setState({ customFields: update(this.state.customFields, { name: 'New Field', type: '', required: true, validation: {} }) })
+  }
+
+  updateCustomField (index, field) {
+    this.setState({ customFields: update(this.state.customFields, index, field) })
+  }
+
+  deleteCustomField (index) {
+    this.setState({ customFields: remove(this.state.customFields, index) })
+  }
+
+  cleanValidationJson (json) {
+    if (json && Array.isArray(json)) {
+      return json.map((item) => {
+        if (item.validation) {
+          const newItemValidation = Object.assign({}, item.validation)
+          Object.keys(newItemValidation).forEach((field) => {
+            const fieldValue = newItemValidation[field]
+            newItemValidation[field] = fieldValue === null || fieldValue === 'null' || fieldValue === '' ? '' : fieldValue
+          })
+          item.validation = newItemValidation
+        }
+        return item
+      })
+    }
+    return json
+  }
+
+  saveCustomFields () {
+    fetch(`/regi/settings/custom-fields`,
+      { method: 'POST',
+        body: JSON.stringify(this.state.customFields),
+        headers: {
+          'Accept': 'application/json',
+          'Content-Type': 'application/json'
+        },
+        credentials: 'same-origin'
+      })
+      .then((res) => res.json())
+      .then(this.cleanValidationJson)
+      .then((data) => {
+        if (typeof data === 'string') {
+          try {
+            data = JSON.parse(data)
+          } catch (e) {}
+        }
+        if (data.Message && data.Message === 'An error has occurred.') {
+          this.swalSaveErrorAlert()
+        }
+      })
+      .catch(console.error)
+  }
+
+  render () {
+    const footer = (
+      <div className='btn-group'>
+        <button type='button' className='btn btn-success' onClick={this.addCustomField.bind(this)}>Add New Custom Field</button>
+        <button type='button' className='btn btn-success' onClick={this.saveCustomFields.bind(this)}>Save Fields</button>
+      </div>
+    )
+    return (
+      <Box
+        type='success'
+        title='Custom Fields'
+        subtitle='Other fields required for client user accounts'
+        footer={footer}
+      >
+        <LoadableOverlay loaded={this.state.loaded} />
+        {this.state.customFields.map((f, i) => (
+          <CustomField
+            field={f}
+            key={i}
+            index={i}
+            onDelete={this.deleteCustomField.bind(this)}
+            onUpdate={this.updateCustomField.bind(this)}
+          />
+        ))}
+      </Box>
+    )
+  }
+}
+
+export default CustomFields
