0a1c4e5ed69ae1f2c4b3abc1a0c7bb84eb5a1e08
Michael <michaelfiford@gmail.com>
2017-04-10 15:34:53 +0100

    Error handling for missing settings

---
+++ Registrar.Models/Exceptions/InvalidSettingException.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Registrar.Models.Exceptions
+{
+    public class InvalidSettingException : Exception
+    {
+        public InvalidSettingException(string type, string name, string value)
+            : base($"Invalid {type} value for setting {name}: {value}")
+        {
+        }
+    }
+}
