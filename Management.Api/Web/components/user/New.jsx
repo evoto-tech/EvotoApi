6093658221fc02ece82249b3443108113db658aa
Matthew Elphick <matt.a.elphy@gmail.com>
2017-04-12 16:45:58 +0100

    Update new user component to show custom fields and actually register

--- Management.Api/Web/components/user/New.jsx
+++ Management.Api/Web/components/user/New.jsx
@@ -1,10 +1,93 @@
 import React from 'react'
 import Wrapper from './parts/Wrapper.jsx'
 import RegisterForm from '../parts/forms/Register.jsx'
+import NamedInput from '../parts/form-components/NamedInput.jsx'
+import cleanValidationJson from '../../lib/clean-validation-json'
 
 class UserNew extends React.Component {
+  constructor (props) {
+    super(props)
+    this.cleanValidationJson = cleanValidationJson
+    this.state = { loaded: false, customFields: [], user: {} }
+  }
+
+  componentDidMount () {
+    fetch(`/regi/settings/custom-fields`, { credentials: 'same-origin' })
+      .then((res) => res.json())
+      .then(this.cleanValidationJson)
+      .then((data) => {
+        const user = data.reduce((user, field) => Object.assign(user, { [field.name]: '' }), {})
+        this.setState({ customFields: data, loaded: true, user })
+      })
+      .catch(console.error)
+  }
+
   register (user, successLink, handleError) {
-    console.log('what', user)
+    user.CustomFields = this.state.customFields.map((field) => {
+      if (this.state.user.hasOwnProperty(field.name) && field.type === 'Date') {
+        return { name: field.name, value: moment(this.state.user[field.name]).format() }
+      } else if (this.state.user.hasOwnProperty(field.name)) {
+        return { name: field.name, value: this.state.user[field.name] }
+      }
+    })
+    fetch('/regi/account/register',
+      { method: 'POST',
+        body: JSON.stringify(user),
+        headers: {
+          'Accept': 'application/json',
+          'Content-Type': 'application/json'
+        },
+        credentials: 'same-origin'
+      })
+      .then((response) => {
+        if (response.ok && response.status === 200) {
+          // Do nothing
+        } else if (response.status === 401) {
+          throw new Error('Those details seem to be wrong! Please try again.')
+        } else if (response.status === 400) {
+          return response.json()
+        } else {
+          throw new Error('An unknown error occurred')
+        }
+      }, handleError)
+      .then((data) => {
+        if (data) {
+          let messages = data.ModelState
+              ? Object.keys(data.ModelState).reduce((o, k) => data.ModelState[k], [])
+              : undefined
+          return handleError(data, messages)
+        } else {
+          if (successLink) this.props.router.push(successLink)
+        }
+      }, handleError)
+      .catch(handleError)
+  }
+
+  updateField (field, type, e) {
+    let value = e.target.value
+    const user = Object.assign({}, this.state.user, { [field]: value })
+    this.setState({ user })
+  }
+
+  renderCustomFields () {
+    if (!this.state.customFields || this.state.customFields.length < 1) return ''
+    const elements = this.state.customFields.reduceRight((elements, element, i) => {
+      return [].concat([ element ]).concat([ 'br' ]).concat(elements)
+    })
+    return elements.map((field, i) => {
+      if (typeof field === 'string') return <br key={i} />
+      let type = (field.type || 'text').toLowerCase()
+      if (type === 'string') type = 'text'
+      return (
+        <NamedInput
+          key={i}
+          name={field.name}
+          type={type}
+          value={this.state.user[field.name]}
+          onChange={this.updateField.bind(this, field.name, field.type)}
+        />
+      )
+    })
   }
 
   render () {
@@ -21,7 +104,11 @@ class UserNew extends React.Component {
               onProcess={this.register.bind(this)}
               successLink='/users'
               buttonText='Create New User'
-            />
+            >
+              <div>
+                {this.renderCustomFields()}
+              </div>
+            </RegisterForm>
           </div>
         </div>
       </Wrapper>
