f36cfc8002c8cb8d912b9f5574afb84e2a15ab39
Matthew Elphick <matt.a.elphy@gmail.com>
2017-04-13 16:04:42 +0100

    Add management user create route and request model

---
+++ Management.Models/Request/CreateManaUser.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Runtime.Serialization;
+
+namespace Management.Models.Request
+{
+    [DataContract]
+    public class CreateManaUser
+    {
+        [DataMember(Name = "email")]
+        [Required]
+        [EmailAddress]
+        public string Email { get; private set; }
+
+        [DataMember(Name = "password")]
+        [DataType(DataType.Password)]
+        [Required]
+        public string Password { get; private set; }
+
+        [DataMember(Name = "confirmPassword")]
+        [DataType(DataType.Password)]
+        [Compare("Password", ErrorMessage = "Entered Passwords do not match")]
+        [Required]
+        public string ComparePassword { get; private set; }
+    }
+}
