252e7ca6b4839f4cbddac4966feb5b77ec00b64d
Michael <michaelfiford@gmail.com>
2017-04-13 15:46:31 +0100

    Add blockchain host in response configured in settings

--- Common/RegiSettings.cs
+++ Common/RegiSettings.cs
@@ -4,5 +4,11 @@
     {
         public static string REGISTER_ENABLED = "User Registration";
         public static bool REGISTER_ENABLED_DEFAULT = true;
+
+#if DEBUG
+        public static string BLOCKCHAIN_HOST = "localhost";
+#else
+        public static string BLOCKCHAIN_HOST = "api.evoto.tech";
+#endif
     }
 }
